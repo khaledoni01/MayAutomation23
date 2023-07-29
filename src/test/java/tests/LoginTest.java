f9bc2be61097488e992658b3c78340359df4f88f
khaledoni01 <khaledoni01@gmail.com>
2023-07-29 11:29:09 -0400

    added maven plugin

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,7 +1,9 @@
 package tests;
 
+import junit.framework.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.HomePage;
 import pages.LoginPage;
 
@@ -11,17 +13,26 @@ public class LoginTest extends BaseTest{
     public static Object[][] loginTestData() {
 
         return new Object[][] {
-                {"admin", "admin"},
-                {"khaledzamanqa", "test1234"},
-                {"admin12", "admin12"},
-                {"khaled12", "test1234"}
+                {"admin", "admin"}
+//                {"khaledzamanqa", "test1234"},
+//                {"admin12", "admin12"},
+//                {"khaled12", "test1234"}
         };
     }
 
     @Test(groups = {"Smoke", "Regression"}, dataProvider = "loginData")
     public void loginInvalidUsernameInvalidPassword(String username, String password){
         LoginPage loginPage = new LoginPage();
+
+        SoftAssert sAssert = new SoftAssert();
+
         loginPage.login(username, password);
+
+        sAssert.assertEquals(getDriver().getCurrentUrl(), "https://google.com", "url mismatched" );
+
+        System.out.println("After the assert");
+
+        sAssert.assertAll();
     }
 
 
